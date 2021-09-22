cf10fc30b7f27250b33e51e07602052bfd39a1ec
Roel Tombozafy <rtombozafy96@gmail.com>
2021-09-22 18:30:25 +0300

    Added services and constants folder

---
+++ lib/constants/urls.dart
@@ -0,0 +1,1 @@
+const BASE_URL = 'https://demo-digispeed.fr/api/index.php';
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' show Client;
+import 'package:app_dolibarr/constants/urls.dart';
+
+class AuthService {
+  Client client = Client();
+
+  Future<String> login(String username, String password) async {
+    final response = await client
+        .post(Uri.parse('$BASE_URL/login?login=$username&password=$password'));
+    final body = json.decode(response.body);
+  }
+}
